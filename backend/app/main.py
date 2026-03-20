33f7979b10e41cde6da66b1d13624ac81c9b3f45
Sheroz Melikov <sherozmelikov043@gmail.com>
2026-03-20 00:29:52 +0000

    Update CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,12 +31,13 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(movies_router, prefix="/movies", tags=["Movies"])
 app.include_router(users_router, prefix="/users", tags=["Users"])
 # app.include_router(interactions.router, prefix="/interactions", tags=["Interactions"])
