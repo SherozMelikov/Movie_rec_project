155cfb2a27d94a13c39388a06558c8ced807dfb0
Sheroz Melikov <sherozmelikov043@gmail.com>
2026-03-20 00:50:57 +0000

    Add frontend URL to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://movie-rec-project.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
